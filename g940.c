3b40250ac1125df03a0013a329d75c55c2622b38
fred41 <haro41@gmx.de>
2018-09-11 12:49:51 +0200

    hid report processing simplified

--- g940.c
+++ g940.c
@@ -1,12 +1,6 @@
 #include "g940.h"
 
 using namespace std;
-// one complete cache line
-uint32_t state_buf[16] __attribute__((aligned(64)))= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-
-int uinput_fd = 0;
-libusb_context *usb_ctx = NULL;
-libusb_device_handle *g940_handle;
 
 // lookup-table for BTN codes
 const uint16_t btn_code_lut[32] = {
@@ -48,6 +42,26 @@ const uint16_t btn_code_lut[32] = {
 	0					// external power supply connected, not mapped to event
 };
 
+// the HAT looup-table, extended to 16 just to be safe
+const hat_xy hat_lut[16] = {
+	{0,-1},
+	{1,-1},
+	{1,0},
+	{1,1},
+	{0,1},
+	{-1,1},
+	{-1,0},
+	{-1,-1},
+	{0,0},
+	{0,0},
+	{0,0},
+	{0,0},
+	{0,0},
+	{0,0},
+	{0,0},
+	{0,0}
+};
+
 const uint8_t axes_code_lut[20] = {
 // main axes x,y, 10bit each, -512..511
 	ABS_X,
@@ -77,25 +91,11 @@ const uint8_t axes_code_lut[20] = {
 	ABS_TILT_X,
 	ABS_TILT_Y
 };
-// the HAT looup-table, extended to 16 just to be safe
-const hat_xy hat_lut[16] = {
-	{0,-1},
-	{1,-1},
-	{1,0},
-	{1,1},
-	{0,1},
-	{-1,1},
-	{-1,0},
-	{-1,-1},
-	{0,0},
-	{0,0},
-	{0,0},
-	{0,0},
-	{0,0},
-	{0,0},
-	{0,0},
-	{0,0}
-};
+
+libusb_device_handle *g940_handle = NULL;
+libusb_context *usb_ctx = NULL;
+int uinput_fd = 0;
+
 
 void g940_find_usb_dev(libusb_device **devs, size_t cnt, libusb_device **g940_dev, libusb_device_handle **g940_handle) {
 
@@ -252,120 +252,110 @@ void sig_cb_handler(int signum) {
 	exit(signum);
 }
 
-int g940_process_report(libusb_device_handle *handle, int ui_fd) {
+void g940_process_report(int ui_fd, uint32_t* state_buf) {
 	uint32_t *state_diff = &state_buf[6], *state = &state_buf[11];
 	uint32_t mask;
-	int size = 0, i , j;
+	int i , j;
 	struct input_event event;
 //	timespec ts, te;
 
-	if (libusb_interrupt_transfer(handle, LIBUSB_ENDPOINT_IN | G940_ENDPOINT,
-		(uint8_t*)state_buf + 3, G940_REPORT_SIZE, &size, G940_READ_TIMEOUT)) {
+//	clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
 
-		printf("libusb_interrupt_transfer failed");
-		return 1;
-
-	} else {
-//		clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-
-		if (size != G940_REPORT_SIZE) {
-			printf("unexpected report size : %d\n", size);
-			return 1;
-		}
-		for (i=0; i<5; i++) {
-			state_diff[i] = state[i] ^ state_buf[i + 1];
-			state[i] = state_buf[i + 1];
-		}
-
-		gettimeofday(&event.time, 0);
-
-		event.type = EV_ABS;
-		// process 32bit wise, then refine, for performance
-		// main axes X, Y
-		if (*state_diff) {
-			// main axes X, Y
-			if (*state_diff & 0b1111111111) {
-				event.code = axes_code_lut[0];
-				event.value = (*state & 0b1111111111) << 22;
-				event.value /= 0x400000;
-				write(ui_fd, &event, sizeof(event));
-			}
-			if (*state_diff & 0b11111111110000000000) {
-				event.code = axes_code_lut[1];
-				event.value = (*state & 0b11111111110000000000) << 12;
-				event.value /= 0x400000;
-				write(ui_fd, &event, sizeof(event));
-			}
-			// HAT0, HAT1, HAT2
-			if (*state_diff & 0xfff00000) {
-				mask = 0x00f00000;
-				for (int i=0; i<3; i++) {
-					if (*state_diff & mask) {
-						uint8_t index = (*state & mask) >> (i * 4 + 20);
-						event.code = axes_code_lut[i * 2 + 2];
-						event.value = hat_lut[index].x;
-						write(ui_fd, &event, sizeof(event));
-						event.code = axes_code_lut[i * 2 + 3];
-						event.value = hat_lut[index].y;
-						write(ui_fd, &event, sizeof(event));
-					}
-					mask = mask << 4;
-				}
-			}
-		}
-
-		// THROTTLE 1&2, RUDDER, BRAKE RIGHT
-		// BRAKE LEFT, TRIM 3&2&1
-		// R1, R2 , MINI_X, MINI_Y
-		for (i=0; i<3; i++) {
-			state_diff++; state++;
-			if (*state_diff) {
-				mask = 0xff;
-				for (j=0; j<4; j++) {
-					if (*state_diff & mask) {
-						event.code = axes_code_lut[i * 4 + j + 8];
-						event.value = (*state & mask) >> (j * 8);
-						write(ui_fd, &event, sizeof(event));
-					}
-					mask = mask << 8;
-				}
-			}
-		}
-
-		// all the other buttons, sensors and switches
-		if (++state_diff) {
-			state++;
-			mask = 1;
-			event.type = EV_KEY;
-			for (i=0; i<32; i++) {
-				if(*state_diff & mask) {
-					event.code = btn_code_lut[i];
-					if(event.code) {
-						event.value = (*state & mask) >> i;
-						write(ui_fd, &event, sizeof(event));
-					}
-				}
-				mask = mask << 1;
-			}
-		}
-
-		event.type = EV_SYN; event.code = SYN_REPORT; event.value = 0;
-		write(ui_fd, &event, sizeof(event));
+	for (i=0; i<5; i++) {
+		state_diff[i] = state[i] ^ state_buf[i + 1];
+		state[i] = state_buf[i + 1];
 	}
+
+	gettimeofday(&event.time, 0);
+
+	event.type = EV_ABS;
+	// process 32bit wise, then refine, for performance
+	// main axes X, Y
+	if (*state_diff) {
+		// main axes X, Y
+		if (*state_diff & 0b1111111111) {
+			event.code = axes_code_lut[0];
+			event.value = (*state & 0b1111111111) << 22;
+			event.value /= 0x400000;
+			write(ui_fd, &event, sizeof(event));
+		}
+		if (*state_diff & 0b11111111110000000000) {
+			event.code = axes_code_lut[1];
+			event.value = (*state & 0b11111111110000000000) << 12;
+			event.value /= 0x400000;
+			write(ui_fd, &event, sizeof(event));
+		}
+		// HAT0, HAT1, HAT2
+		if (*state_diff & 0xfff00000) {
+			mask = 0x00f00000;
+			for (int i=0; i<3; i++) {
+				if (*state_diff & mask) {
+					uint8_t index = (*state & mask) >> (i * 4 + 20);
+					event.code = axes_code_lut[i * 2 + 2];
+					event.value = hat_lut[index].x;
+					write(ui_fd, &event, sizeof(event));
+					event.code = axes_code_lut[i * 2 + 3];
+					event.value = hat_lut[index].y;
+					write(ui_fd, &event, sizeof(event));
+				}
+				mask = mask << 4;
+			}
+		}
+	}
+
+	// THROTTLE 1&2, RUDDER, BRAKE RIGHT
+	// BRAKE LEFT, TRIM 3&2&1
+	// R1, R2 , MINI_X, MINI_Y
+	for (i=0; i<3; i++) {
+		state_diff++; state++;
+		if (*state_diff) {
+			mask = 0xff;
+			for (j=0; j<4; j++) {
+				if (*state_diff & mask) {
+					event.code = axes_code_lut[i * 4 + j + 8];
+					event.value = (*state & mask) >> (j * 8);
+					write(ui_fd, &event, sizeof(event));
+				}
+				mask = mask << 8;
+			}
+		}
+	}
+
+	// all the other buttons, sensors and switches
+	if (++state_diff) {
+		state++;
+		mask = 1;
+		event.type = EV_KEY;
+		for (i=0; i<32; i++) {
+			if(*state_diff & mask) {
+				event.code = btn_code_lut[i];
+				if(event.code) {
+					event.value = (*state & mask) >> i;
+					write(ui_fd, &event, sizeof(event));
+				}
+			}
+			mask = mask << 1;
+		}
+	}
+
+	event.type = EV_SYN; event.code = SYN_REPORT; event.value = 0;
+	write(ui_fd, &event, sizeof(event));
 
 //	clock_gettime(CLOCK_MONOTONIC_RAW, &te);
 //	double dur = ((double)te.tv_nsec - (double)ts.tv_nsec) / 1000;
 //	printf("us: %f\n",dur);
 
-	return 0;
 }
 
 int main(int argc, char** argv) {
 
+	// one complete cache line
+	static uint32_t state_buf[16] __attribute__((aligned(64)))= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	libusb_device **devs = NULL;
-	libusb_device *g940_dev;
+	libusb_device *g940_dev = NULL;
+
 	size_t cnt = 0;
-	int ret = 0;
+	int ret = 0, size;
 	bool running = true;
 
 	ret = libusb_init(&usb_ctx);
@@ -394,9 +384,22 @@ int main(int argc, char** argv) {
 		return 1;
 
 	signal(SIGINT, sig_cb_handler);
+
 	do {
-		if (g940_process_report(g940_handle, uinput_fd))
+		if (libusb_interrupt_transfer(g940_handle, LIBUSB_ENDPOINT_IN | G940_ENDPOINT,
+			(uint8_t*)state_buf + 3, G940_REPORT_SIZE, &size, G940_READ_TIMEOUT)) {
+
+			printf("libusb_interrupt_transfer failed");
 			running = false;
+		} else {	
+
+			if (size != G940_REPORT_SIZE)
+				printf("unexpected report size : %d\n", size);
+
+			else 
+				g940_process_report(uinput_fd, state_buf);
+		}
+		
 	} while (running);
 
 	cleanup();
